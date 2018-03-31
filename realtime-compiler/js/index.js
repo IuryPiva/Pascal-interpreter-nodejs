6d6f8983f8c2e63af622955cd1a4b6eef6f735e2
dericksm <derick_sm@hotmail.com>
2018-03-31 14:56:26 -0300

    css compiler

---
+++ realtime-compiler/js/index.js
@@ -0,0 +1,7 @@
+// Editor Settings (Provided by C9)
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.setShowPrintMargin(false);
+editor.getSession().setMode("ace/mode/pascal");
+editor.focus();
+
