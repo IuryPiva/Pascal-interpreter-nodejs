5461a524862993c4bb00823f7d5d6f88bad2ac79
Iury Piva <iurypiva@gmail.com>
2018-04-10 10:52:45 -0300

    reorder folder merge branch2

--- src/utils/text-utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    getCharacters: function(text) {
-        return text.split('').reverse()
-    },
-    removeComments: function(text) {
-        return text.replace(/(\(\*([\s\S]*?)\*\))/gm, '');
-    }
-}
