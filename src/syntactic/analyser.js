7e25ef84cd17cadaa5036928a8f2198ce1038dc5
Iury Piva <iurypiva@gmail.com>
2018-04-14 10:45:15 -0300

    ignore

--- src/syntactic/analyser.js
+++ src/syntactic/analyser.js
@@ -2,30 +2,33 @@ const tokenTypes = require('../utils/token-types')
 const parserMatrix = require('../utils/parser-matrix')
 
 module.exports = function (tokenStack) {
-    let derivateStack = [{token: tokenTypes.getToken('PROGRAMA'), word: 'PROGRAMA'}]
-    while (derivateStack.length > 0) {
-        const derivate = derivateStack[0]
-        const entry = tokenStack[0]
+  // DERIVATE É X // ENTRY É A
+  let derivateStack = [{
+    token: tokenTypes.getToken('PROGRAMA'),
+    word: 'PROGRAMA'
+  }]
+  while (derivateStack.length > 0) {
+    const derivate = derivateStack[0]
+    const entry = tokenStack[0]
 
-        if(tokenTypes.isTerminal(x)) {
-            if(derivate == entry) {
-                derivateStack.shift()
-                tokenStack.shift()
-            } else {
-                console.log('Deu erro maluko')
-                return
-            }
-        } else {
-            const derivation = parserMatrix.getDerivation(derivate, entry)
-            if(derivation) {
-                derivate.shift()
-                derivateStack.unshift(derivation)
-            } else {
-                console.log('Deu erro maluko')
-                return
-            }
-        }
-        
+    if(tokenTypes.isTerminal(derivate)) {
+      if (derivate == entry) {
+        derivateStack.shift()
+        tokenStack.shift()
+      } else {
+        console.log('Deu erro maluko')
+        return
+      }
+    } else {
+      const derivation = parserMatrix.getDerivation(derivate, entry)
+      if (derivation) {
+        derivate.shift()
+        derivateStack.unshift(derivation)
+      } else {
+        console.log('Deu erro maluko')
+        return
+      }
     }
-    return tokenStack
+  }
+  return tokenStack
 }
