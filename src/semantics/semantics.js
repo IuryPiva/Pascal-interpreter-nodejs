8c0a310ae506d14a9caa9562aba934252d88e66a
dericksm <derick_sm@hotmail.com>
2018-05-27 13:33:13 -0300

    7 dias de greve e o Iury ainda nao fez nada

--- src/semantics/semantics.js
+++ src/semantics/semantics.js
@@ -9,17 +9,22 @@ let begin = false
 let end = false
 let index = 0
 
+let arrayToken = false
+let arrayVar = []
+
 module.exports = function (tokenStack) {
 
-    function identifierExists(newIdentifier, level) {
-        identifiers.forEach(identifier => {
-            if (newIdentifier.word == identifier.word && identifier.level <= level) {
-                return true
-            }
-
-        })
-        return false
-    }
+  function identifierExists(newIdentifier, level){
+      
+    identifiers.forEach(element => {
+        debugger
+        if (newIdentifier.word.toLowerCase() == element.token.word.toLowerCase() && element.level <= level) {
+            return true
+        } else {
+            return false
+        }
+      });
+  }
 
     function removeByLevel(l) {
         for (let i = 0; i < identifiers.length; i++) {
@@ -31,17 +36,66 @@ module.exports = function (tokenStack) {
         }
     }
 
-    function typeVerification(token) {
-    }
-
     function returnLastLevel(array) {
         return array[array.length - 1]
     }
+    
+    function verifyInteger(){
+        if(tokenStack[index+1].word.toLowerCase() == '=' && tokenStack[index +2].token == '26'){
+            return true
+            
+        } else {
+            return false
+        }
+        
+    }
 
-    
-    
+    function verifyArrray(token){
+        debugger
+        let l = index
+                
+                while(tokenStack[l].word != ';'){
+                    if(tokenStack[l].token == '25'){
+                        arrayVar.push(tokenStack[l])
+                    }
+                    l--
+                }
+            
+        
 
-    
+        arrayVar.forEach(token => {
+            token.category = 'array'
+            token.type = 'integer'
+            identifiers.push({ token, level })
+        });
+        
+    }
+
+    function verifyType(token) {
+        if(token.category == 'const') {
+            if(verifyInteger()){
+                console.log('verify é true')
+                console.log('é inteiro')
+                
+            }
+        }
+    }
+
+    function identifierDeclaration(token) {
+        if (tokenTypes.isIdentifier(token.token)) {
+
+            if (identifierExists(token, level)) {
+                console.log('entrou')
+                verifyType(token)
+                console.log("identificador existe")
+            } else {
+                tokenStack[index].category = category.toLowerCase()
+                identifiers.push({ token, level })
+                verifyType(token)
+            }
+        }
+    }
+        
     tokenStack.forEach(token => {
 
         if (token.word.toLowerCase() == 'program' || token.word.toLowerCase() == 'case' || token.word.toLowerCase() == 'procedure' || token.word.toLowerCase() == 'if' || token.word.toLowerCase() == 'while' || token.word.toLowerCase() == 'for') {
@@ -150,16 +204,6 @@ module.exports = function (tokenStack) {
             }
         }
         
-        
-        // if (token.word.toLowerCase() == 'end') {
-
-        //     removeByLevel(level)
-        //     level--
-
-        // }
-
-
-
         if (tokenTypes.isCategory(token.token)) {
             category = token.word
         }
@@ -168,23 +212,37 @@ module.exports = function (tokenStack) {
             category = ""
         }
 
+        if(token.token == '9') {
+            
+            verifyArrray(token)
+        }
+
         if (tokenTypes.isIdentifier(token.token)) {
             if (category != "") {
-                tokenStack[index].category = category
+                tokenStack[index].category = category.toLowerCase()
                 identifiers.push({ token, level })
+                verifyType(token)
+                
+
             } else {
                 if (identifierExists(token, level)) {
-                    //console.log("identificador existe")
+                    console.log('entrou')
+                    verifyType(token)
+                    console.log("identificador existe")
                 } else {
-                    // console.log("identificador nao existe")
+                    console.log("identificador nao existe")
                 }
             }
         }
         index++
+        
+
+
 
 
     });
-    console.log(level)
+    
+    
     level = -1
     index = 0
 }
